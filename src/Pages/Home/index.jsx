3fedb94755266239f421a09c370c7bf698e6465d
Ricardo Lence <lencericardo@gmail.com>
2019-04-22 19:09:24 +0200

    navegacion incluida

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import Nav from './nav.jsx';
+
+class Home extends Component{
+
+       render(){
+              return(
+                     <Nav />
+              )
+       }
+};
+
+export default Home
